4393648675935a84492dc17d8ac06dfe4eeccb5a
Wahab Alshahin <wahab@sboinc.com>
2026-06-27 19:01:34 -0400

    Add py.typed marker and re-export public schema types for consumer type hints

--- src/ajera/__init__.py
+++ src/ajera/__init__.py
@@ -1,5 +1,100 @@
 from ajera.client import AjeraClient
+from ajera.schemas.client import (
+    Client,
+    ClientDetails,
+    ClientType,
+    UpdatedClientResult,
+)
+from ajera.schemas.contact import (
+    Contact,
+    ContactDetails,
+    ContactType,
+    UpdatedContactResult,
+)
+from ajera.schemas.deduction import Deduction
+from ajera.schemas.employee import (
+    Employee,
+    EmployeeDetails,
+    EmployeeType,
+    UpdatedEmployeeResult,
+)
+from ajera.schemas.fringe import Fringe
+from ajera.schemas.ledger import LedgerAccount, LedgerAccountDetails
+from ajera.schemas.project import (
+    Project,
+    ProjectTemplate,
+    ProjectTemplateDetails,
+    ProjectTotalsDetails,
+    ProjectType,
+)
+from ajera.schemas.project_summary import ProjectSummary
+from ajera.schemas.project_v2 import ProjectBundle
+from ajera.schemas.reference import (
+    AccountGroup,
+    Activity,
+    BankAccount,
+    ChargeablePhase,
+    Company,
+    Department,
+    InvoiceFormat,
+    Pay,
+    PayrollTax,
+    RateTable,
+    WageTable,
+)
+from ajera.schemas.vendor import (
+    UpdatedVendorResult,
+    Vendor,
+    VendorDetails,
+    VendorType,
+)
+from ajera.schemas.vendor_invoice import (
+    VendorInvoice,
+    VendorInvoiceBundle,
+    VendorInvoiceLineItemCreate,
+)
 
 __all__ = [
+    "AccountGroup",
+    "Activity",
     "AjeraClient",
+    "BankAccount",
+    "ChargeablePhase",
+    "Client",
+    "ClientDetails",
+    "ClientType",
+    "Company",
+    "Contact",
+    "ContactDetails",
+    "ContactType",
+    "Deduction",
+    "Department",
+    "Employee",
+    "EmployeeDetails",
+    "EmployeeType",
+    "Fringe",
+    "InvoiceFormat",
+    "LedgerAccount",
+    "LedgerAccountDetails",
+    "Pay",
+    "PayrollTax",
+    "Project",
+    "ProjectBundle",
+    "ProjectSummary",
+    "ProjectTemplate",
+    "ProjectTemplateDetails",
+    "ProjectTotalsDetails",
+    "ProjectType",
+    "RateTable",
+    "UpdatedClientResult",
+    "UpdatedContactResult",
+    "UpdatedEmployeeResult",
+    "UpdatedVendorResult",
+    "Vendor",
+    "VendorDetails",
+    "VendorInvoice",
+    "VendorInvoiceBundle",
+    "VendorInvoiceLineItemCreate",
+    "VendorType",
+    "WageTable",
 ]
